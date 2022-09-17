e9fc3e793455acecebfaa0099075fecd0f461822
Andres <brinezlopez08@gmail.com>
2022-09-17 12:17:05 -0500

    cambiar el controller en habilitando las anotaciones de seguridad

--- Proyecto/src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ Proyecto/src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
+//@SpringBootApplication (exclude ={SecurityAutoConfiguration.class}) para que  no  nos  pida  autenticación al iniciar el proyecto, si debe quitar cuando se necesita hacer autenticacion
+@SpringBootApplication
+
 public class Ciclo3Application {
 
 
