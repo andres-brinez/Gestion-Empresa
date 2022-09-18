0f1bc4b028f2bf5c341e1e955d627e980c69d49b
Andres <brinezlopez08@gmail.com>
2022-09-17 23:05:50 -0500

    crear  nuevo respository  para traer  el nombre del usuario con su username

--- Proyecto/src/main/java/com/UdeA/Ciclo3/repo/UsuarioRepository.java
+++ Proyecto/src/main/java/com/UdeA/Ciclo3/repo/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.UdeA.Ciclo3.repo;
 
 import com.UdeA.Ciclo3.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -13,4 +14,8 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     // trae la información que está en  la tabla relacionada con la entidad Empresa
     // hereda el  JpaRepository que es eliminar, editar, guardar, actualizar, buscar por id, buscar todos, etc
 
+
+    //Metodo que me trae el nombre de un usuario cuando tengo su user_name
+    @Query(value="SELECT nombre FROM usuario where user_name=?1", nativeQuery = true)
+    public abstract String NamePorUserName(String user_name);
 }
