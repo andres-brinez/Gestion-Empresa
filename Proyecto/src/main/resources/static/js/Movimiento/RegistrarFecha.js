828be9330a3d58f63456377fb36f66d0b6b93a7c
Andres <brinezlopez08@gmail.com>
2022-09-20 22:37:38 -0500

    Cambiar código para generar fechas

--- Proyecto/src/main/resources/static/js/Movimiento/RegistrarFecha.js
+++ Proyecto/src/main/resources/static/js/Movimiento/RegistrarFecha.js
@@ -1,5 +1,14 @@
 // registrar fecha de movimiento automaticamente
 window.onload = function() { // al cargar la página
-    // traer la fehca actual y la pasa a json, solo trae los numeros del 1 a 10 y remplaza los / por -
-    document.getElementById("fechaRegistro").value=new Date().toJSON().slice(0,10).replace(/-/g,'-');
+
+    // traer fecha actual en formato "yyyy-MM-dd"
+    let fechaActual = new Date().toISOString().slice(0,10);
+
+    // campo fecha
+    let campoFecha= document.getElementById("fechaRegistro")
+
+    campoFecha.value = fechaActual;
+
+
+
 }
