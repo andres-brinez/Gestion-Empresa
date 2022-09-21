7a3149a83b688ad48d41fa9e6a8707399ec14181
Andres <brinezlopez08@gmail.com>
2022-09-21 00:16:29 -0500

    Crear alertas y organizar el código

--- Proyecto/src/main/resources/static/js/Empresa/ValidarBotones.js
+++ Proyecto/src/main/resources/static/js/Empresa/ValidarBotones.js
@@ -1,52 +1,46 @@
 
-//Validar los botones que hay en la vista de Empresa
+//---------Validar los botones que hay en la vista de Empresa---------------------
 
-// seleccionar solo un checkbox
-let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+// seleccionar solo un checkbox, llama la función creada
+soloUnCheckbox()
 
-checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-        checkboxes.forEach(function(otherCheckbox) {
-            if (otherCheckbox !== checkbox)
-                otherCheckbox.checked = false
-        })
-    })
-})
-
-
-//boton Ver empleados
-const buttonEmpleados = document.querySelector('.btnEmpleados');
-
-buttonEmpleados.addEventListener('click', function(e){
-
-    try{
-
-        // saber cual checkbox esta seleccionado
-        let checkboxSeleccionado = document.querySelector('input[type="checkbox"]:checked');
-        // el value del checkbox es el id de la empresa
-        let idEmpresa = checkboxSeleccionado.value;
-
-
-        //redireciona a la página indicada con el atributo id
-        window.location.href = "/Empresa/"+idEmpresa+"/Empleados";
-
-
-    }
-    catch (e) {
-
-        alert("No hay ninguna Empresa  seleccionada");
-    }
-
-
-
-
-
-
-});
+// al dar click en el boton
 
 const buttonEliminar = document.querySelector(".btnEliminar")
-buttonEliminar.addEventListener("click", function(e) {
+buttonEliminar.addEventListener("click", function(e) {EditarEliminar("eliminar" )})
 
+const buttonEditar = document.querySelector('.btnEditar');
+buttonEditar.addEventListener('click', function(e){EditarEliminar("editar")});
+
+
+// Ver los movimientos de la empresa seleccionada
+const buttonMovimientosEmpresa = document.querySelector(".btnMovimientos")
+buttonMovimientosEmpresa.addEventListener("click", function(e) {EmpleadoMovimiento("Movimientos");})
+
+//ver los empleados de la empresa seleccionada
+const buttonEmpleadosEmpresa = document.querySelector('.btnEmpleados-Empresa');
+buttonEmpleadosEmpresa.addEventListener('click', function(e){EmpleadoMovimiento("Empleados")});
+
+
+//------------------------FUNCIONES---------------------
+
+// función para seleccionar solo un checkbo
+function soloUnCheckbox() {
+
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+            checkboxes.forEach(function(otherCheckbox) {
+                if (otherCheckbox !== checkbox)
+                    otherCheckbox.checked = false
+            })
+        })
+    })
+}
+
+// con tipo indicamos si es editar o eliminar
+function EditarEliminar(tipo){
 
     try{
 
@@ -56,73 +50,79 @@ buttonEliminar.addEventListener("click", function(e) {
         // el value del checkbox es el id de la empresa
         let idEmpresa = checkboxSeleccionado.value;
 
-        // confirmar si se quiere eliminar la empresa
-        let confirmacion = confirm("¿Está seguro que desea eliminar la empresa?");
-        if(confirmacion){
+        if (tipo == "eliminar") {
+
+            Swal.fire({
+                title: "¿Estás seguro que desea eliminar esta empresa?",
+                text: "No podras revertir esto!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si',
+                cancelButtonText: 'Cancelar'
+            })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        //redireciona a la página indicada con el atributo id
+                        window.location.href = "/EliminarEmpresa/" + idEmpresa;
+                    }
+
+                })
+        }
+        else if (tipo == "editar") {
+
+            // redireciona a la página indicada con el atributo id
+            window.location.href = "/EditarEmpresa/" + idEmpresa;
+        }
+
+
+
+
+    }
+
+    catch (e) {
+        // si no hay ningun checkbox seleccionado
+        Swal.fire({
+            title: 'No hay ninguna empresa seleccionada',
+            text: "Seleccione una para poder realizar esta acción",
+            icon: 'question',
+
+        })
+    }
+
+}
+
+// con tipo indicamos si es para Empleados o Movimientos
+function EmpleadoMovimiento(tipo){
+    try{
+
+        // saber cual checkbox esta seleccionado
+        let checkboxSeleccionado = document.querySelector('input[type="checkbox"]:checked');
+        // el value del checkbox es el id de la empresa
+        let idEmpresa = checkboxSeleccionado.value;
+
+        if(tipo== "Empleados"){
             //redireciona a la página indicada con el atributo id
-            window.location.href = "/EliminarEmpresa/"+idEmpresa;
+            window.location.href = "/Empresa/"+idEmpresa+"/Empleados";
+        }
+
+        else if (tipo== "Movimientos") {
+            //redireciona a la página indicada con el atributo id
+            window.location.href = "/Empresa/"+idEmpresa+"/Movimientos";
         }
 
 
 
     }
     catch (e) {
+        // si no hay ningun checkbox seleccionado
+        Swal.fire({
+            title: 'No hay ninguna empresa seleccionada',
+            text: "Seleccione una  para poder realizar esta acción",
+            icon: 'question',
 
-        alert("No hay ninguna Empresa  seleccionada");
+        })
     }
 
-
-})
-
-//boton editar Empresa
-const buttonEditar = document.querySelector('.btnEditar');
-
-buttonEditar.addEventListener('click', function(e){
-
-    try{
-
-        // saber cual checkbox esta seleccionado
-        let checkboxSeleccionado = document.querySelector('input[type="checkbox"]:checked');
-        // el value del checkbox es el id de la empresa
-        let idEmpresa = checkboxSeleccionado.value;
-
-
-        //redireciona a la página indicada con el atributo id
-        window.location.href = "/EditarEmpresa/"+idEmpresa;
-
-
-    }
-    catch (e) {
-
-        alert("No hay ninguna Empresa  seleccionada");
-    }
-
-});
-
-// Ver los movimientos de la empresa seleccionada
-const buttonMovimiento = document.querySelector(".btnMovimientos")
-buttonMovimiento.addEventListener("click", function(e) {
-
-
-    try{
-
-        // saber cual checkbox esta seleccionado
-        let checkboxSeleccionado = document.querySelector('input[type="checkbox"]:checked');
-
-        // el value del checkbox es el id de la empresa
-        let idempleado = checkboxSeleccionado.value;
-
-
-        //redireciona a la página indicada con el atributo id
-
-        window.location.href = "Empresa/"+idempleado+"/Movimientos";
-
-
-    }
-    catch (e) {
-
-        alert("Selecciona la empresa para ver sus momovimientos");
-    }
-
-
-})
+}
